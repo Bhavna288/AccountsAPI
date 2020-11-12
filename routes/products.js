98772c0286afc67ef3a001f9184f54e309f36f2f
unknown <bhavnatahelyani288@gmail.com>
2020-11-12 13:00:58 +0530

    Bcrypt

--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+// const bcrypt = require('bcryptjs');
 const Product = require('../models/Product');
+const jwt = require('jsonwebtoken');
 
 // Validation
 const Joi = require('@hapi/joi');
 
-const schema = {
+const schema = Joi.object({
     title: Joi.string().min(3).required(),
     description: Joi.string(),
     price: Joi.number().required().greater(0)
-}
+});
 
 // Gets back all the products
 router.get('/', async (req, res) => {
@@ -35,24 +36,37 @@ router.get('/:productId', async (req, res) => {
 // Submits or creates a product
 router.post('/', async (req, res) => {
 
-    const { error } = Joi.validate(req.body, schema);
+    const { error } = schema.validate(req.body);
 
-    if (error) {
-        res.status(400).send(error.details[0].message);
-    } else {
-        const product = new Product({
-            title: req.body.title,
-            description: req.body.description,
-            price: req.body.price
-        });
+    if (error) return res.status(400).send(error.details[0].message);
 
-        try {
-            const savedProduct = await product.save()
-            res.json(savedProduct);
-        } catch (err) {
-            res.status(400).send(err);
-        }
+    // Checking if the product is already in the database
+    const titleExist = await Product.findOne({ title: req.body.title });
+    console.log("titleExist");
+    if (titleExist) return res.status(400).send("Product already exists.");
+
+    // Hash the password
+    // const salt = await bcrypt.genSalt(10);
+    // const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Validate the password
+    // const validPass = await bcrypt.compare(req.body.password, titleExist.password);
+    // if (!validPass) return res.status(400).send("Incorrect password.");
+
+    const product = new Product({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price
+    });
+
+    try {
+        const savedProduct = await product.save()
+        // res.json(savedProduct);
+        res.json({ productId: product._id });
+    } catch (err) {
+        res.status(400).send(err);
     }
+
 });
 
 // Delete a product
