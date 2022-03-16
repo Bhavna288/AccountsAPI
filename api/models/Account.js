6025db1affe76a6690ff35dca69733daa5851436
Bhavna288 <bhavnatahelyani288@gmail.com>
2022-03-16 16:35:52 +0530

    Database string updated

--- api/models/Account.js
+++ api/models/Account.js
@@ -4,7 +4,6 @@ const AccountSchema = mongoose.Schema({
     accountName: {
         type: String,
         required: true,
-        unique: true,
     },
     accountType: {
         type: mongoose.Schema.Types.ObjectId,
