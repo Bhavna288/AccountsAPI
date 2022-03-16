d01d83fa91c8a9eacd2acd63eae55297b5e3f3e5
Bhavna288 <bhavnatahelyani288@gmail.com>
2022-03-16 12:20:02 +0530

    messages updated

--- api/messages/message.js
+++ api/messages/message.js
@@ -1,18 +1,23 @@
+const today = new Date().toLocaleString('en-GB', { timeZone: 'Asia/Kolkata' }).replace(/\//g, "-");
+today = today.replace(',', "");
+const prefix = today + " divya-traders-api: ";
+
+
 module.exports = {
     addMessage: (module) => {
-        console.log(module, " added");
+        console.log(prefix, module, " added");
         return (module + " added successfully");
     },
     deleteMessage: (module) => {
-        console.log(module, " deleted");
+        console.log(prefix, module, " deleted");
         return (module + " deleted successfully");
     },
     updateMessage: (module) => {
-        console.log(module, " updated");
+        console.log(prefix, module, " updated");
         return (module + " updated successfully");
     },
     noRecord: (module) => {
-        console.log(module, " record not found");
+        console.log(prefix, module, " record not found");
         return (module + " does not exists or is deleted.");
     }
 }
