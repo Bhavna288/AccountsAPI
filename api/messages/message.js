8c392e94cbb58ba8459cdbc23f1537c9c183fda4
Bhavna288 <bhavnatahelyani288@gmail.com>
2022-03-16 12:09:06 +0530

    messages console logged

--- api/messages/message.js
+++ api/messages/message.js
@@ -1,14 +1,18 @@
 module.exports = {
     addMessage: (module) => {
+        console.log(module, " added");
         return (module + " added successfully");
     },
     deleteMessage: (module) => {
+        console.log(module, " deleted");
         return (module + " deleted successfully");
     },
     updateMessage: (module) => {
+        console.log(module, " updated");
         return (module + " updated successfully");
     },
     noRecord: (module) => {
+        console.log(module, " record not found");
         return (module + " does not exists or is deleted.");
     }
 }
